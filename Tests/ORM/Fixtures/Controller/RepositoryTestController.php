8d80245c6317dc948dd263495804c9e48831a021
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2017-04-01 21:11:30 +0200

    kristofer: Removed unnecessary code

--- Tests/ORM/Fixtures/Controller/RepositoryTestController.php
+++ Tests/ORM/Fixtures/Controller/RepositoryTestController.php
@@ -8,9 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-
-use AppBundle\Entity\TestEntity;
 
 class RepositoryTestController extends Controller {
 
