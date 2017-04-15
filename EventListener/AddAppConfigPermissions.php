6f0e9c6bd9281fca23d0005fe7b20643c9c22465
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2017-04-15 11:13:26 +0200

    kristofer: Made some preparations

---
+++ EventListener/AddAppConfigPermissions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ordermind\LogicalAuthorizationBundle\EventListener;
+
+use Ordermind\LogicalAuthorizationBundle\Event\AddPermissionsEvent;
+
+class AddDoctrinePermissions {
+    public function onAddPermissions(AddPermissionsEvent $event) {
+
+    }
+}
--- Services/PermissionTreeManagerInterface.php
+++ Services/PermissionTreeManagerInterface.php
@@ -3,7 +3,6 @@
 namespace Ordermind\LogicalAuthorizationBundle\Services;
 
 interface PermissionTreeManagerInterface {
-  public function generateTree();
   public function getTree();
   public function mergePermissions($arrays = []);
 }
