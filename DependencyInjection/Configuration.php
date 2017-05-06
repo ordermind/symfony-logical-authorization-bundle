5c252c1857dc0a5cbc37425ef912f7cb920dedb6
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2017-05-06 16:57:11 +0200

    kristofer: Removed doctrine-related services

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,15 +22,9 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-                ->booleanNode('check_lazy_loaded_models')
-                  ->defaultFalse()
-                  ->end()
                 ->variableNode('permissions')->end()
             ->end()
         ;
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
