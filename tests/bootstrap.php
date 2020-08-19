09040e6a3b09d2765a1cf2d70cea28420128843a
Kristofer Tengström <info@kristofer-tengstrom.se>
2020-08-19 18:18:43 +0200

    Update deprecated code in bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,21 +2,6 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-$file = __DIR__ . '/../vendor/autoload.php';
-if (!file_exists($file)) {
-    $file = __DIR__ . '/../../../../vendor/autoload.php';
-    if (!file_exists($file)) {
-        throw new RuntimeException('Install dependencies to run test suite.');
-    }
-}
-
-$autoload = require $file;
-AnnotationRegistry::registerLoader([$autoload, 'loadClass']);
-
-// --------------BOOT KERNEL--------------
-
 require_once __DIR__ . '/AppKernel.php';
 
 $kernel = new Ordermind\LogicalAuthorizationBundle\Test\AppKernel('test', true); // create a "test" kernel
